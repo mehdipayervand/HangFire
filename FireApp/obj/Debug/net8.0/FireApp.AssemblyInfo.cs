c7e36e634d41f6ca75cef65470ca42e32b795583
Mehdi Payervand <mehdipayervand@gmail.com>
2024-10-25 00:01:29 +0330

    init

--- FireApp/obj/Debug/net8.0/FireApp.AssemblyInfo.cs
+++ FireApp/obj/Debug/net8.0/FireApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FireApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1a8b2e1c29d83224304cce4a0e664ce3e711e6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FireApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FireApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
